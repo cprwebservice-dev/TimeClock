5783865a9117265a0714ec27104c1c21022082a7
cprwebservice-dev <cprwebservice@gmail.com>
2026-09-18 11:43:50 +0700

    Add files via upload

--- portal-sw.js
+++ portal-sw.js
@@ -1,8 +1,8 @@
-const CACHE='timeattendance-portal-v61529-fix16ag';
+const CACHE='timeattendance-portal-v61529-fix16ah';
 const SHELL=[
   './portal.html',
-  './portal.css?v=6.15.29-fix16ag-text-dark-palette-20260918',
-  './portal.js?v=6.15.29-fix16ag-text-dark-palette-20260918',
+  './portal.css?v=6.15.29-fix16ah-dark-toggle-20260918',
+  './portal.js?v=6.15.29-fix16ah-dark-toggle-20260918',
   './favicon.svg'
 ];
 
